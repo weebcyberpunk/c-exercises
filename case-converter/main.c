1b04da3ac83bf62df39edcd8e135c15e44698f2e
GG <weebcyberpunk@gmail.com>
2022-03-07 20:35:40 -0300

    Added case converter

---
+++ case-converter/main.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+
+/*
+ * author GG weebcyberpunk@gmail.com
+ * version 0.0.0
+ * since Mar 07, 2022
+ */
+
+/*
+ * Converts lower case to upper case with -lu
+ * Converts upper case to lower case with -ul
+ * Reverts the case with -r
+ */
+int main(int argv, char *argc[]) {
+
+	char c;
+
+	if (argv == 1) {
+		printf("Usage:\n");
+		printf("-lu 	to convert lower to upper\n");
+		printf("-ul 	to convert upper to lower\n");
+		printf("-r 	to revert case\n");
+		return(1);
+	}
+	
+	if (strcmp(argc[1], "-lu") == 0) {
+		for (;;) {
+			c = getchar();
+			if (islower(c)) c = toupper(c);
+			if (c == EOF) break;
+			printf("%c", c);
+
+		}
+
+	} else if (strcmp(argc[1], "-ul") == 0) {
+		for (;;) {
+			c = getchar();
+			if (isupper(c)) c = tolower(c);
+			if (c == EOF) break;
+			printf("%c", c);
+
+		}
+	
+	} else if (strcmp(argc[1], "-r") == 0) {
+		for (;;) {
+			c = getchar();
+			if (isupper(c)) c = tolower(c);
+			else if (islower(c)) c = toupper(c);
+			if (c == EOF) break;
+			printf("%c", c);
+
+		}
+	}
+
+	return(0);
+}
