d68c26a0ff9408f5b1565a17e78342c7dc6c668a
GG <weebcyberpunk@gmail.com>
2022-03-07 21:07:12 -0300

    Added linebreaker

---
+++ linebreaker/main.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+ * author GG weebcyberpunk@gmail.com
+ * version 0.0.0
+ * since Mar 07, 2022
+ */
+
+/*
+ * Reads stdin, breaks lines based on args
+ */
+int main(int argv, char *argc[]) {
+
+	char c;
+	int x = 0;
+	int lw;
+
+	if (argv > 1) {
+		lw = atoi(argc[1]);
+
+	} else {
+		printf("Please specify a number of chars to break the line.\n");
+		return(1);
+
+	}
+	
+	for (;;) {
+		c = getchar();
+		if (c == EOF) break;
+		if (c == '\n') continue;
+		printf("%c", c);
+		x++;
+		if (x == lw) {
+			printf("\n");
+			x = 0;
+
+		}
+	}
+	
+	printf("\n");
+	
+	return(0);
+}
