aed8e9324f4d766eef7227990741e02c13bf0b4a
GG <weebcyberpunk@gmail.com>
2022-03-10 00:00:14 -0300

    Added line-word-breaker

---
+++ line-word-breaker/main.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define IN 1
+#define OUT 0
+
+/*
+ * author GG weebcyberpunk@gmail.com
+ * version 0.0.0
+ * since Mar 09, 2022
+ */
+int main() {
+
+	char c;
+	int state = OUT;
+	for (;;) {
+		c = getchar();
+		if (c == EOF) {
+			break;
+
+		} else if ((c == '\t' || c == ' ' || c == '\n')) {
+			if (state == IN) { 	// we need to do two if because
+				printf("\n"); 	// this way it wont print the second
+				state = OUT; 	// blank in a roll
+			}
+
+		} else {
+			printf("%c", c);
+			state = IN;
+
+		}
+	}
+
+	printf("\n");
+	
+	return(0);
+}
