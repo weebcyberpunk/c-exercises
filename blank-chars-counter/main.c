e2b39b05bafaa370504e6c1e3d2c708d6d13fa25
GG <weebcyberpunk@gmail.com>
2022-03-09 23:43:57 -0300

    Added blank chars counter

---
+++ blank-chars-counter/main.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+ * author GG weebcyberpunk@gmail.com
+ * version 0.0.0
+ * since Mar 09, 2022
+ */
+
+/*
+ * Counts blanks, tabs, spaces and newlines
+ */
+int main() {
+
+	char c;
+	int blanks = 0;
+	int tabs = 0;
+	int spaces = 0;
+	int newlines = 0;
+
+	for (;;) {
+		c = getchar();
+		if (c == EOF) break;
+		else if (c == 255) blanks++;
+		else if (c == '\t') tabs++;
+		else if (c == ' ') spaces++;
+		else if (c == '\n') newlines++;
+
+		printf("%c", c);
+
+	}
+
+	printf("\n\n");
+	printf("blanks: %i\n", blanks);
+	printf("tabs: %i\n", tabs);
+	printf("spaces: %i\n", spaces);
+	printf("newlines: %i\n", newlines);
+	printf("total: %i\n", (blanks + tabs + spaces + newlines));
+	
+	return(0);
+}
