9fe6c16dae7574995c507c21590cd81dfacf72d1
GG <weebcyberpunk@gmail.com>
2022-03-10 00:20:11 -0300

    Added cat

---
+++ cat/main.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+ * author GG weebcyberpunk@gmail.com
+ * version 0.0.0
+ * since Mar 10, 2022
+ */
+
+/*
+ * Nothing much to say about
+ * Concatenate files to stdout, and if
+ * no args, concatenates from stdin
+ */
+int cat(FILE *fp) {
+	char c;
+	for (;;) {
+		c = fgetc(fp);
+		if (c == EOF) break;
+		printf("%c", c);
+
+	}
+
+	return(0);
+}
+
+int main(int argv, char *argc[]) {
+
+	FILE *fp;
+	if (argv == 1) {
+		cat(stdin);
+
+	} else {
+
+		for (int c = 1; c < argv; c++) {
+			fp = fopen(argc[c], "r");
+			if (fp == NULL) {
+				fprintf(stdout, "Cannot open file %s\n", argc[c]);
+
+			} else {
+				cat(fp);
+
+			}
+		}
+	}
+
+	printf("\n");
+	
+	return(0);
+}
