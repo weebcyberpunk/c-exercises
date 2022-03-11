a478fed63e06f65fec3e99b21b13331fb0a43296
GG <weebcyberpunk@gmail.com>
2022-03-11 00:28:50 -0300

    Added detab

---
+++ detab/main.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+ * author GG weebcyberpunk@gmail.com
+ * version 0.0.0
+ * since Mar 11, 2022
+ */
+
+/*
+ * Concatenates stdin to stdout,
+ * replacing every tab with as much
+ * as spaces as defined by args
+ */
+int main(int argv, char *argc[]) {
+
+	char c;
+	int spaces;
+
+	// get spaces
+	if (argv > 1) {
+		spaces = atoi(argc[1]);
+
+	} else {
+		fprintf(stderr, "Please specify tab size.\n");
+		return(1);
+
+	}
+
+	// main loop
+	for (;;) {
+		c = getchar();
+		if (c == EOF) break;
+
+		if (c == '\t')
+			for (int count = 0; count < spaces; count++) 
+				printf(" ");
+
+		else printf("%c", c);
+
+	}
+
+	printf("\n");
+	
+	return(0);
+}
