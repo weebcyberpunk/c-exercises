cf31085b1e3c511682bd7662ce5c7b635e35692c
GG <weebcyberpunk@gmail.com>
2022-04-19 23:02:24 -0300

    Added blank replacer improved

---
+++ blank-replacer-improved/main.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define BLANK 1
+#define NOT_BLANK 0
+#define NEWLINE 1
+#define NOT_NEWLINE 0
+
+/*
+ * author GG weebcyberpunk@gmail.com
+ * version 0.0.0
+ * since Apr 19, 2022
+ */
+
+/*
+ * Concatenates stdin to stdout, replacing sequences of blanks with only one
+ * blank and removing entire blank lines
+ */
+int main() {
+
+	char c;
+	int blank = NOT_BLANK;
+	int newline = NEWLINE;
+
+	for (;;) {
+
+		c = getchar();
+		if (c == EOF) break;
+
+		if ((c == ' ') || (c == '\t')) {
+
+			if (blank) continue;
+			blank = BLANK;
+			newline = NOT_NEWLINE;
+
+		} else if (c == '\n') {
+
+			if (newline) continue;
+			newline = NEWLINE;
+			blank = NOT_BLANK;
+
+		} else {
+
+			newline = NOT_NEWLINE;
+			blank = NOT_BLANK;
+		}
+
+		printf("%c", c);
+	}
+
+	printf("\n");
+
+	return(0);
+}
